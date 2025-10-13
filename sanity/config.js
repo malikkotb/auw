ffff5bca6436a1681a094e561e395195352bea90
Malik <malikkotb@icloud.com>
2025-10-13 13:34:47 +0200

    deploy sanity studio

--- sanity/config.js
+++ sanity/config.js
@@ -1,22 +1,11 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
-// export const config = {
-//   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-//   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-//   apiVersion:
-//     process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-10-06",
-//   useCdn: process.env.NODE_ENV === "production",
-//   token: process.env.SANITY_API_TOKEN,
-// };
-
-// TODO: make these environment variables
-
 export const client = createClient({
-  projectId: "04o7eam1",
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: false,
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  apiVersion: process.env.SANITY_API_VERSION || "2024-01-01",
+  useCdn: process.env.NODE_ENV === "production",
 });
 
 // Set up image builder
